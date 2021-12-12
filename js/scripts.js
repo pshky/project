b058077080770024a3d89bb70eb1b63b370ccd47
Prasiddha Shakya <shakyaprasiddha8@gmail.com>
2021-12-12 13:54:43 +0545

    GRUNT

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+<script>
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                $("#mycarousel").carousel('pause');
+                $("#carouselButton").children("span").removeClass('fa-pause');
+                $("#carouselButton").children("span").addClass('fa-play');
+            }
+            else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');                    
+            }
+        });
+});
+$(document).ready(function() {
+$("#myBtn").click(function() {
+    $("#myModal").modal("show");
+});
+});
+</script>
